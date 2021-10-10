b72895dee02eb3f589fe7ff137fb9db0ed6c2352
StarKing Develoeprs <88481114+sanultemp@users.noreply.github.com>
2021-10-10 11:50:03 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config:
+    BOT_USE = False
+    BOT_TOKEN = ''                         # from @botfather
+    APP_ID =                               # from https://my.telegram.org/apps
+    API_HASH = ''                          # from https://my.telegram.org/apps
+    AUTH_USERS = []
